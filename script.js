2e7cfc109ec4cc89cc119503e8d67c436ccdf76e
Kinsasha <kennedyokechukwu203@gmail.com>
2025-06-04 13:41:48 +0100

    I added the button that lets you pick a number and edits the squares accordingly.
    Up next, I will add a limitation on the number of
    squares that it can add

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 const divContainer = document.querySelector(".container-one");
 
 const GRIDSIDE = 600;
-const rows = 16;
-const columns = 16;
-
-//GRIDSIDE.className = "";
+// let rows = 16;
+// let columns = 16;
 
 divContainer.style.height = `${GRIDSIDE}px`;
 divContainer.style.width = `${GRIDSIDE}px`;
 
-function createGridBoxes() {
-  for (let i = 0; i < rows * columns; i++) {
+function createGridBoxes(squaresPerSide = 16) {
+  divContainer.innerHTML = "";
+  for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
     const gridBoxes = document.createElement("div");
     gridBoxes.className = "boxes";
 
-    gridBoxes.style.height = `${GRIDSIDE / rows - 2}px`;
-    gridBoxes.style.width = `${GRIDSIDE / columns - 2}px`;
+    gridBoxes.style.height = `${GRIDSIDE / squaresPerSide - 2}px`;
+    gridBoxes.style.width = `${GRIDSIDE / squaresPerSide - 2}px`;
+
+    gridBoxes.addEventListener("mouseover", changeBoxColor);
+
+    function changeBoxColor() {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+
+      const color = `rgb(${r}, ${g}, ${b})`;
+      gridBoxes.style.backgroundColor = color;
+    }
 
     divContainer.appendChild(gridBoxes);
   }
 }
+
 createGridBoxes();
+
+const squareButton = document.querySelector(".squares");
+
+squareButton.addEventListener("click", changeSquareNumber);
+
+function changeSquareNumber() {
+  const square = Number(prompt("Enter a number"));
+  if (isNaN(square) || square <= 0) {
+    alert("please input a number greater than 0");
+    return;
+  }
+  createGridBoxes(square);
+}
